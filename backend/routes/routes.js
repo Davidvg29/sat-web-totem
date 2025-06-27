c7b9038b8910d70b7fdf40acfd93702ec4e33eb3
Davidvg29 <davidpivito@gmail.com>
2025-06-27 12:12:41 -0300

    agrego ruta para imprimir factura

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const getInmueble = require('../controllers/inmueble/getInmueble');
+const printFacturaPdf = require('../controllers/factura/printFacturaPdf');
 
 //inmueble
 router.get("/inmueble/:codInmueble", getInmueble)
+
+//factura
+router.get("/factura/imprimir/:numeroFactura", printFacturaPdf)
 
 module.exports = router;
