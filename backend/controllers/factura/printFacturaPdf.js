616870fbf64cb5705eadaaf5fe77154b58409d9a
Lucas <lucasmcoronel@gmail.com>
2025-10-22 12:26:22 -0300

    se agrega front de novedades y se borra los .json

--- backend/controllers/factura/printFacturaPdf.js
+++ backend/controllers/factura/printFacturaPdf.js
@@ -6,9 +6,10 @@ const pool = require("../../config/bd/bd");
 
 const printFacturaPdf = async (req, res) => {
     const {codInmueble, emision, prefijo, numFactura, periodo, vencimiento} = req.body;
+    console.log(req.body);
+    
 
     const filePath = path.join(__dirname, `../../cache/res_facturas_vigentes${prefijo}${numFactura}.pdf`);
-    const jsonPath = path.join(__dirname, "../../data/data.json");
 
     let conn = null;
 
@@ -17,49 +18,25 @@ const printFacturaPdf = async (req, res) => {
         await getFacturasVigentesSAT(`res_facturas_vigentes${prefijo}${numFactura}.pdf`, conn);
         await printer.print(filePath, { printer: process.env.IMPRESORA });
 
-        // Leer JSON
-        const data = await fs.readFile(jsonPath, "utf-8");
-        const dataJson = JSON.parse(data);
-
-        // Actualizar cantidad total
-        dataJson.facturasImpresasTotal = String(Number(dataJson.facturasImpresasTotal) + 1);
-
-        // Obtener fecha actual en formato YYYY-MM-DD
-        const hoy = new Date().toISOString().split("T")[0];
-
-        // Buscar si ya hay registro para hoy
-        const registroExistente = dataJson.facturasImpresas.find(f => f.fecha === hoy);
-
-        if (registroExistente) {
-            // Si ya existe, sumar 1
-            registroExistente.cantidad = String(Number(registroExistente.cantidad) + 1);
-        } else {
-            // Si no existe, agregar nuevo registro
-            dataJson.facturasImpresas.push({
-                fecha: hoy,
-                cantidad: "1"
-            });
-        }
-
         //Enviar respuesta
         res.status(200).json({
             status: true,
             message: "Factura impresa correctamente.",
         });
 
-        //Guardar cambios en el JSON
-        await fs.writeFile(jsonPath, JSON.stringify(dataJson, null, 2), "utf-8");
-
         //Eliminar el PDF temporal
         await fs.unlink(filePath);
-
+        
         const querySearchFactura = await pool.query(`select * from facturas_impresas where numero_factura = $1`, [numFactura])
         if (querySearchFactura.rowCount > 0) {
             await pool.query(`update facturas_impresas set cantidad_impresion = cantidad_impresion + 1 where numero_factura = $1`, [numFactura])
         }else{
             const queryInsert = `INSERT INTO facturas_impresas(codigo_inmueble, periodo_factura, prefijo_factura, numero_factura, emision_factura, vencimiento_factura, cantidad_impresion)
-                VALUES($1, $2, $3, $4, $5, $6, $7)` 
-            await pool.query(queryInsert, [codInmueble, periodo, prefijo, numFactura, emision, vencimiento, 1])
+                VALUES($1, $2, $3, $4, $5, $6, $7) returning*` 
+               
+                
+           const a = await pool.query(queryInsert, [codInmueble, periodo, prefijo, numFactura, emision, vencimiento, 1])
+            
         }
 
         await pool.query(`update total_facturas_impresas set cantidad_total = cantidad_total + 1 `)
--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: 'http://150.150.150.108:3000'
+  baseURL: 'http://localhost:3000'
 });
 
 export default api
