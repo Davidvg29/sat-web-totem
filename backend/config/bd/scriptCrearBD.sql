713c0a2ae5091bdab3849a63f228eccba43df697
Davidvg29 <davidpivito@gmail.com>
2025-10-22 11:27:14 -0300

    cambio scrip de bd

--- backend/config/bd/scriptCrearBD.sql
+++ backend/config/bd/scriptCrearBD.sql
@@ -6,11 +6,15 @@ create table facturas_impresas(
 	id_factura_impresa serial primary key,
 	codigo_inmueble integer,
 	periodo_factura integer,
+	prefijo_factura integer,
 	numero_factura integer,
+	emision_factura varchar(10),
+	vencimiento_factura varchar(10),
 	fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	cantidad_impresion integer
 )
 
 create table total_facturas_impresas(
+	id_cantidad_total serial primary key,
 	cantidad_total integer
 )
